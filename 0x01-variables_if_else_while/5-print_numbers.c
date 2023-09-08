4bc45aa598f40e8c5d5aefec9fe68ce6309c36a2
Clear008 <wissal-exo@hotmail.com>
2023-09-08 08:54:36 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int Num[]={0,1,2,3,4,5,6,7,8,9};
+int i;
+
+for(i = 0 ; i < 10 ; i++)
+{
+printf("%d", Num[i]);
+}
+return (0);
+}
