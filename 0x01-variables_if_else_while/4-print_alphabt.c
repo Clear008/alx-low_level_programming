bbb98a72a8bfe5ba5ec1d37a441186d33794aaa1
Clear008 <wissal-exo@hotmail.com>
2023-09-08 08:30:28 -0700

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alphabetLowcase[] = "abcdefghijklmnopqrstuvwxyz";
+
+int i;
+
+for ( i = 0 ; i < 26 ; i++ )
+{
+if ( i!=4 && i!=16 )
+putchar(alphabetLowcase[i]);
+}
+putchar('\n');
+
+return (0);
+}
