9f4cbdbece562d689de447dd54294ba99fe9a303
Clear008 <wissal-exo@hotmail.com>
2023-09-08 07:06:06 -0700

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+char alphabetLowcase[] = "abcdefghijklmnopqrstuvwxyz";
+char alphabetUppercase[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+int i;
+
+for (i = 0 ; i < 32 ; i++)
+
+putchar(alphabetLowcase[i]);
+putchar(alphabetUppercase[i]);
+putchar('\n');
+
+return (0);
+}
