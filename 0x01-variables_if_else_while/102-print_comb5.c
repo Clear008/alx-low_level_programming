cc0056f4b8d52194bc0264e9aece19349d6677ce
Clear008 <wissal-exo@hotmail.com>
2023-09-08 19:19:40 -0700

    102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - Entry point
  *
@@ -6,14 +6,19 @@
  */
 int main(void)
 {
-int i;
-int j;
-for (i = 0 ; i <= 98 ; i++)
-{	
-for (j = i + 1 ; j <= 99 ; j++)
+int i, j;
+for (i = 0; i <= 98; i++)
 {
-putchar(i + '0');
-putchar(j + '0');
+for (j = i + 1; j <= 99; j++)
+{
+if (i != j)
+{
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
+putchar(' ');
+putchar((j / 10) + '0');
+putchar((j % 10) + '0');
+
 if (!(i == 98 && j == 99))
 {
 putchar(',');
@@ -21,6 +26,8 @@ putchar(' ');
 }
 }
 }
+}
 putchar('\n');
+
 return (0);
 }
