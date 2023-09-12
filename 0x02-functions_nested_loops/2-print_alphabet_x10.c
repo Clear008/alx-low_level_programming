05e1a27ea055d383c0eccbeffdbdf9fdfcb1b955
Clear008 <wissal-exo@hotmail.com>
2023-09-12 11:57:11 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_alphabet -  the alphabet a - z
+ * print_alphabet_x10 -  the alphabet a - z ten times 
  *
 */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 
 int i;
