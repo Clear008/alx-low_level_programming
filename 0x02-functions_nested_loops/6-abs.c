d7dd3fde78db9cc9c621591e2ea0ec872d64eed5
Clear008 <wissal-exo@hotmail.com>
2023-09-12 14:50:17 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_abs  - the absolute value of an integer
+ *@a: integer
+ *Return: absolute value
+*/
+
+int _abs(int = a)
+
+if (a >= 0)
+{
+return (n);
+}
+else
+return (-n);
+
