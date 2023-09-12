3320279f168b8b8843c3d3f52455c35eb735f10b
Clear008 <wissal-exo@hotmail.com>
2023-09-12 12:26:18 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include<type.h>
+/**
+ * print _islower
+ *
+ * return value of '_islower'
+*/
+
+int _islower(int c)
+{
+int i;
+if (i islower)
+{
+putchar(49);
+}
+else
+{
+putchar(48);
+
+}
+_putchar('\n');
+}
+
