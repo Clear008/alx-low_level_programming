26ba4435ebf1c88b738f710574cd15b25f00a2e7
Clear008 <wissal-exo@hotmail.com>
2023-09-12 16:08:50 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,12 +11,12 @@ int i;
 int j;
 for (i = 0 ; i <= 23 ; i++)
 {
-for (j =i + 1 ; j <= 59 ; j++)
+for (j = i + 1 ; j <= 59 ; j++)
 _putchar ((i / 10) + '0');
 _putchar((i % 10) + '0');
 _putchar(':');
 _putchar((j / 10) + '0');
 _putchar((j % 10) + '0');
-
+_putchar('\n');
 }
 }
