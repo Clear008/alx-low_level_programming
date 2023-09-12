4b958ee800c492a9c98fa775f98999e8ce2fbb98
Clear008 <wissal-exo@hotmail.com>
2023-09-12 14:28:52 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,25 +12,19 @@ int print_sign(int n)
 if (n > 0)
 {
 
-putchar('+');
-putchar(',');
-putchar(' ');
-}
-return (1);
+_putchar('+');
 
+return (1);
+}
 else if (n < 0)
 {
 
-putchar('-');
-putchar(',');
-putchar(' ');
+_putchar('-');
 return (-1);
 }
 else
 {
-putchar('0');
-putchar(',');
-putchar(' ');
+_putchar('0');
 
 return (0);
 }
