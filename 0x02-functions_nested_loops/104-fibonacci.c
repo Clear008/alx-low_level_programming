328c89dd3d50cb5bfcffd227970f66a497a07f96
Clear008 <wissal-exo@hotmail.com>
2023-09-13 16:24:07 -0700

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+long int n1 = 1, n2 = 2, n3;
+
+int i, n = 98;
+printf("%ld, %ld", n1, n2);
+for (i = 3; i <= n; ++i)
+{
+
+n3 = n1 + n2;
+if (i < n)
+{
+printf(", %ld", n3);
+}
+else
+{
+printf(", %ld", n3);
+printf("\n");
+}
+n1 = n2;
+n2 = n3;
+
+}
+
+return (0);
+}
+
