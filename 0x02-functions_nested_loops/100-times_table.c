a01390757bafea01305bbf4236af1ae676b3e589
Clear008 <wissal-exo@hotmail.com>
2023-09-13 11:46:10 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,32 +13,32 @@ return;
 
 for (i = 0; i <= n; i++)
 {
-for (j = 0; j <= n; j++)
-{
-int mul = i * j;
-if (j < n)
+for (j = 1; j <= n; j++)
 {
 _putchar(',');
 _putchar(' ');
-_putchar(' ');
-_putchar(' ');
-}
+
+int mul = i * j;
 
 if (mul <= 9)
-{
-if (j > 0)
-{
-_putchar(' ');
-}
-_putchar(mul + '0');
-}
 
-else
-{
+_putchar(' ');
+
+if (mul <= 99)
+
+_putchar(' ');
+
+if (mul >= 100)
+_putchar((mul / 100) + '0');
+_putchar((mul / 10) % 10 + '0');
+
+
+if else (mul <= 99 && mul >= 10)
+
 _putchar((mul / 10) + '0');
 _putchar((mul % 10) + '0');
 }
-}
 _putchar('\n');
+}
 }
 }
