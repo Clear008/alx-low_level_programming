77747c3b105d9b919ded2f41d6237c1bf9e67b12
Clear008 <wissal-exo@hotmail.com>
2023-09-13 15:07:58 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,16 +6,16 @@
  */
 int main(void)
 {
-long long int n1 = 1, n2 = 2, n3;
+long int n1 = 1, n2 = 2, n3;
 
 int i, n = 50;
-printf("%lld, %lld, ", n1, n2);
+printf("%ld, %ld, ", n1, n2);
 for (i = 3; i <= n; ++i)
 {
 
 n3 = n1 + n2;
 
-printf("%lld, ", n3);
+printf("%ld, ", n3);
 n1 = n2;
 n2 = n3;
 
