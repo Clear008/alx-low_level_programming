109660b9a9f57e13ae8572c7e2b57336799fda7b
Clear008 <wissal-exo@hotmail.com>
2023-09-13 10:49:24 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,51 +16,11 @@ for (i = 0; i <= n; i++)
 for (j = 0; j <= n; j++)
 {
 int mul = i * j;
-
-if (j >= 0)
-{
+_putchar(mul + '0');
 _putchar(',');
 _putchar(' ');
-}
-
-if (mul < 10 && j >= 0)
-{
-_putchar(' ');
-_putchar(' ');
-_putchar(' ');
-}
-else if (mul < 100 && j >= 0)
-{
-_putchar(' ');
-_putchar(' ');
-}
-else if (j >= 0)
-{
-_putchar(' ');
-}
-
-if (mul == 0) 
-{
-putchar('0');
-}
-else
-{
-int temp = mul;
-int digits[3] = {0};
-int count = 0;
-int k;
-
-while (temp > 0)
-{
-digits[count++] = temp % 10;
-temp /= 10;
-}
-for (k = count - 1; k >= 0 ;k --)
-{
-_putchar(digits[k] + '0');
-}
 }
 }
 _putchar('\n');
 }
-}
+
