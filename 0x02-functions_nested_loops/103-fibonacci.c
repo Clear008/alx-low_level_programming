1f58e174e094c51997fb402de468c79af130718c
Clear008 <wissal-exo@hotmail.com>
2023-09-13 15:39:45 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+unsigned long int n1 = 0, n2 = 1, n3;
+float sum;
+
+while (1)
+{
+n3 = n1 + n2;
+
+if ((n3 > 400000)
+break;
+if ((sum % 2) == 0)
+sum += n3;
+n1 = n2;
+n2 = n3;
+}
+printf("%.0f\n", sum);
+
+return (0);
+}
+
