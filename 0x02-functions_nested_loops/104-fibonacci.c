cd9a7723b147bbcfc76bec37ec724b0281d10f6e
Clear008 <wissal-exo@hotmail.com>
2023-09-13 16:47:33 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-#include<stdint.h>
 #include<inttypes.h>
 /**
  * main - Entry point
@@ -11,18 +10,18 @@ int main(void)
 int64_t n1 = 1, n2 = 2, n3;
 
 int i, n = 98;
-printf("%" PRId64", %" PRId64, n1, n2);
+printf("%lld, %lld", n1, n2);
 for (i = 3; i <= n; ++i)
 {
 
 n3 = n1 + n2;
 if (i < n)
 {
-printf(", %" PRId64, n3);
+printf(", %lld", n3);
 }
 else
 {
-printf(", %" PRId64, n3);
+printf(", %lld", n3);
 printf("\n");
 }
 n1 = n2;
