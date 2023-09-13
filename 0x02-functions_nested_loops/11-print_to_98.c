417dc740efc44e51f776bd3dfb77911ec454882a
Clear008 <wissal-exo@hotmail.com>
2023-09-12 18:45:38 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,15 +9,21 @@ void print_to_98(int n)
 {
 int i;
 int j;
+if (n < 98)
+{
 for(i = n; i <= 98 ; n++)
 {
-printf("%d,\t",i);
+printf("%d, ",i);
 }
-
+}
+if (n > 98){
 for(j = n ; j > 98 ; j --)
 {
-printf("%d,\t",j);
+printf("%d, ",j);
 }
-
 }
-
+if (n == 98)
+{
+printf("%d",n);
+}
+}
