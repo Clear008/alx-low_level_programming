86f44c9d225dfe362c98884a8d84a6afdffc516c
Clear008 <wissal-exo@hotmail.com>
2023-09-14 06:46:41 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_numbers -  prints the numbers, from 0 to 9, followed by a new line
+ *
+*/
+void print_numbers(void)
+{
+int Num[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+int i;
+
+for (i = 0 ; i < 10 ; i++)
+{
+_putchar(Num[i] + '0');
+
+}
+_putchar('\n');
+
+}
