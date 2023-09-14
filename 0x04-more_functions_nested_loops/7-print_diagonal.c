5acef5411f73e0b371052da0e7384a9fa7630a05
Clear008 <wissal-exo@hotmail.com>
2023-09-14 09:03:44 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,6 +8,12 @@ void print_diagonal(int n)
 
 int i;
 int j;
+
+if (n <= 0)
+
+_putchar('\n');
+
+
 for (i = 0; i <  n ; i++)
 {
 for (j = 0; j < i ; j++)
@@ -17,11 +23,6 @@ _putchar(' ');
 _putchar('\\');
 _putchar('\n');
 
-if (n == 0)
-{
-_putchar('\n');
-break;
-}
 
 }
 }
