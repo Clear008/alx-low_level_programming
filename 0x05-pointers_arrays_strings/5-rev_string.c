3b62ea8a39807961b01f693c1eb2cfad3ea6c486
Clear008 <wissal-exo@hotmail.com>
2023-09-18 12:21:22 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,7 +9,7 @@ void rev_string(char *s)
 int length = strlen(s);
 int i, j;
 char t;
-for (i = 0 ,j = length - 1 ; i < j ; i++, j--)
+for (i = 0, j = length - 1 ; i < j ; i++, j--)
 {
 t = s[i];
 s[i] = s[j];
