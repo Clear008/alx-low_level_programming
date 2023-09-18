71801956638fb49ae544e9be722d417d1276e5f5
Clear008 <wissal-exo@hotmail.com>
2023-09-18 12:01:14 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include<string.h>
+/**
+ * rev_string - reverses a string
+ *@s: string
+*/
+void rev_string(char *s)
+{
+int length = strlen(s);
+int i;
+for (i = length - 1 ; i >= 0 ; i--)
+}
