44a67f892cdbc7c0ff2e7c50fca7706014a8b2e2
Clear008 <wissal-exo@hotmail.com>
2023-09-18 13:28:51 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,8 +8,8 @@ void puts_half(char *str)
 {
 int n,i;
 int length_of_the_string = strlen(str);
-n = length_of_the_string  / 2;
-for (i = n ; i < length_of_the_string ; i++)
+n = (length_of_the_string - 1) / 2;
+for (i = n + 1 ; i < length_of_the_string ; i++)
 {
 _putchar(str[i]);
 }
