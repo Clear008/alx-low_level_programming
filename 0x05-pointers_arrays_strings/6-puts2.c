56a0c10032be58438e8fc3d145517446c162f85e
Clear008 <wissal-exo@hotmail.com>
2023-09-18 12:46:00 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,16 +7,16 @@
 void puts2(char *str)
 
 {
-int i;
+int i = 0;
+while (str[i] != '\0')
 
-for (i = 0 ; i < 10 ; i++)
-{
 if (i % 2 == 0)
+{
 
-
-_putchar(str[i] + '0');
+_putchar(str[i]);
 
 }
+i++;
 _putchar('\n');
 
 }
