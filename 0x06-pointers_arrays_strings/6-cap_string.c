2292acec37fa9c8da9cfaaa8f3d372909d439a94
Clear008 <wissal-exo@hotmail.com>
2023-09-21 07:02:00 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -20,7 +20,7 @@ if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
 str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}' || i == 0)
 str[i] -= 32;
 i++;
-
-return (str);
 }
+return (str);
+
 }
