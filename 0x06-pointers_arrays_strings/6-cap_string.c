f4d2621d22686057537546e94293f4ed8793344b
Clear008 <wissal-exo@hotmail.com>
2023-09-20 17:52:45 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,20 +9,32 @@ char *cap_string(char *str)
 {
 int i = 0;
 int n = 1;
-
 while (str[i] != '\0')
 {
-if ((str[i] >= 'a' && str[i] <= 'z') && n)
-{
-str[i] -= 32;
-n = 0;
-}
-else if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
+|| str[i] == '.' || str[i] == ',' || str[i] == ';'
+|| str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
 {
 n = 1;
+
+}
+else if (n)
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] -= 32;
+}
+n = 0;
+
 }
 else
 {
+if (str[i] >= 'A' && str[i] <= 'Z')
+{
+str[i] += 32;
+}
+
 n = 0;
 }
 i++;
