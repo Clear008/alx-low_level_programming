9c5107996051d479837cb35d234d6371667e360c
Clear008 <wissal-exo@hotmail.com>
2023-09-25 02:23:42 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*_strchr - locates a character in a string
+*@s: a string
+*@c: a character
+*Return: to the first occurrence of the character c in the string s
+*/
+char *_strchr(char *s, char c)
+{
+char *p = s;
+
+while (*p != '\0')
+{
+if (*p == c)
+{
+return (p);
+}
+p++;
+}
+
+return (NULL);
+}
