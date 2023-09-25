feb82dc0516c7ea00b2fa596114a1331ff72823b
Clear008 <wissal-exo@hotmail.com>
2023-09-25 02:13:50 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_memcpy - copies memory area
+ *@dest: destination pointer
+ *@src: source pointer
+ *@n: bytes from memory area
+ *Return: a pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+int i = 0;
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+return (dest);
+}
