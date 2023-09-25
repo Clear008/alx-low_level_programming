3fd1e9554908a3f6d20160f8a8318816f6855b34
Clear008 <wissal-exo@hotmail.com>
2023-09-25 02:17:35 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,11 +9,14 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i = 0;
-while (src[i] != '\0')
+for (i = 0 ; src[i] != '\0' && i < n ; i++)
 {
 dest[i] = src[i];
-i++;
 }
+for (; i < n ; i++)
+{
 dest[i] = '\0';
+}
+
 return (dest);
 }
