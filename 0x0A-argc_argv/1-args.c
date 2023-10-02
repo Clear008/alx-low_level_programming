407117cd3dc6a8d9e3d81cc6399a73873060e839
Clear008 <wissal-exo@hotmail.com>
2023-10-02 11:06:17 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include <stdlib.h>
+/**
+ *main - check the code
+ *@argv: array of command line argument
+ *@argc: number of command line argument
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+printf("%d\n", argc);
+
+return (0);
+}
+
