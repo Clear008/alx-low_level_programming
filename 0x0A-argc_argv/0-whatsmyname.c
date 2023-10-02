23d1a1ba8c388639696a8b984b69685abf6477ee
Clear008 <wissal-exo@hotmail.com>
2023-10-02 10:49:23 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 /**
  *main - check the code
- *
+ *@argv: string
+ *@argc: integer
  * Return: Always 0.
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	if (argc > 0)
-	printf("%s\n", argv[0]);
+if (argc > 0)
+printf("%s\n", argv[0]);
 
-return (0);			
+return (0);
 }
-
