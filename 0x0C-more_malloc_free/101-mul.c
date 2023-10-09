3fb4bef29d3facf5961a32fa29f6f7a3132f33c4
Clear008 <wissal-exo@hotmail.com>
2023-10-08 19:45:44 -0700

    101-mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -45,46 +45,47 @@ return (length);
  * @argv: array of arguments
  *Return: always 0
  */
-
 int main(int argc, char *argv[])
 {
-int i, l1, l2, length, dig1, dig2, carry, *p, n = 0;
-char *num1, *num2;
-num1 = argv[1], num2 = argv[2];
-if (argc != 3 || !_isdigit(num1) || !_isdigit(num2))
+char *s1, *s2;
+int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+
+s1 = argv[1], s2 = argv[2];
+if (argc != 3 || !_isdigit(s1) || !_isdigit(s2))
 return (98);
-l1 = _strlen(num1);
-l2 = _strlen(num2);
-	length = l1 + l2 + 1;
-	p = malloc(sizeof(int) * length);
-	if (!p)
-		return (1);
-	for (i = 0; i <= l1 + l2; i++)
-		p[i] = 0;
-for (l1 = l1 - 1; l1 >= 0; l1--)
+len1 = _strlen(s1);
+len2 = _strlen(s2);
+len = len1 + len2 + 1;
+result = malloc(sizeof(int) * len);
+if (!result)
+return (1);
+for (i = 0; i <= len1 + len2; i++)
+result[i] = 0;
+for (len1 = len1 - 1; len1 >= 0; len1--)
 {
-dig1 = num1[l1] - '0';
+digit1 = s1[len1] - '0';
 carry = 0;
-for (l2 = _strlen(num2) - 1; l2 >= 0; l2--)
+for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 {
-dig2 = num2[l2] - '0';
-carry += p[l1 + l2 + 1] + (dig1 *dig2);
-p[l1 + l2 + 1] = carry % 10;
+digit2 = s2[len2] - '0';
+carry += result[len1 + len2 + 1] + (digit1 *digit2);
+result[len1 + len2 + 1] = carry % 10;
 carry /= 10;
 }
 if (carry > 0)
-p[l1 + l2 + 1] += carry;
+result[len1 + len2 + 1] += carry;
 }
-for (i = 0; i < length - 1; i++)
+for (i = 0; i < len - 1; i++)
 {
-if (p[i])
-n = 1;
-if (n)
-_putchar(p[i] + '0');
+if (result[i])
+a = 1;
+if (a)
+_putchar(result[i] + '0');
 }
-if (!n)
+if (!a)
 _putchar('0');
 _putchar('\n');
-free(p);
+free(result);
 return (0);
 }
+
