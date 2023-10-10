6ca7e2248a78926069b0033dd36cd8ab50bbdc63
Clear008 <wissal-exo@hotmail.com>
2023-10-10 12:12:13 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -42,8 +42,9 @@ return (dest);
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
-	dog = malloc(sizeof(struct dog_t));
+dog_t *dog;
+
+	dog = malloc(sizeof(dog_t));
 if (dog == NULL)
 	return (NULL);
 dog->name = malloc(_strlen(name) + 1);
