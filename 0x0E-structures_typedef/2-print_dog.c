93cafeb8ab50ce05c52ad89e9d6eed5be0e37882
Clear008 <wissal-exo@hotmail.com>
2023-10-10 10:21:36 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,11 +15,11 @@ printf("Name: (nil)\n");
 
 else
 
-printf("Name:%s\n", d->name);
-printf("Age:%f\n", d->age);
+printf("Name: %s\n", d->name);
+printf("Age: %f\n", d->age);
 if (d->owner == NULL)
 printf("Owner: (nil)\n");
 else
-printf("Owner:%s\n", d->owner);
+printf("Owner: %s\n", d->owner);
 }
 }
