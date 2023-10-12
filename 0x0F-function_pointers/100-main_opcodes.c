c1ff271b454883bc68b922872fc9e8ebaaaa7a6a
Clear008 <wissal-exo@hotmail.com>
2023-10-11 17:50:55 -0700

    100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check the code
+ *@argv: array of arguments
+ *@argc: number of arguments
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+char *f = (char *)main;
+int n;
+
+if (argc != 2)
+	printf("Error\n");
+exit(1);
+n = atoi(argv[1];
+if (n < 0)
+printf("Error\n");
+exit(2);
+while (n--)
+printf("%02hhx%s", *f++, n ? " " : "\n");
+return (0);
+}
+
