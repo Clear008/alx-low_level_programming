35852e27523b8804b7dd5782dacc8bb8bb37e053
Clear008 <wissal-exo@hotmail.com>
2023-10-19 12:03:24 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -8,12 +8,10 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 list_t *tempo;
+list_t *new_node;
 int inx, acount = 0;
 
-if (head == NULL)
-	return (NULL);
-
-list_t *new_node = malloc(sizeof(list_t));
+new_node = malloc(sizeof(list_t));
 if (new_node == NULL)
 	return (NULL);
 for (inx = 0; str[inx] != '\0'; inx++)
@@ -21,13 +19,14 @@ acount++;
 new_node->len = inx;
 new_node->str = strdup(str);
 new_node->next = NULL;
-if (head == NULL)
+if (*head == NULL)
 {
 *head = new_node;
+return (new_node);
 }
-else
 
-*tempo = *head;
+
+tempo = *head;
 while (tempo->next != NULL)
 	tempo = tempo->next;
 tempo->next = new_node;
