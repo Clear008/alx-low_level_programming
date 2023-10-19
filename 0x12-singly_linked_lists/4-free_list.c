2de65911590d92689fbf00976636fd7c927beea3
Clear008 <wissal-exo@hotmail.com>
2023-10-19 12:13:05 -0700

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ * free_list - function that frees a list_t list.
+ * @head: a pointer
+ */
+void free_list(list_t *head)
+{
+if (head == NULL)
+	return;
+if (head->next != NULL)
+	free_list(head->next);
+	free(head->str);
+	free(head);
+}
