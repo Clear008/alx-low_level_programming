21d1735a5fea4b7884a2f1344f4fb350c6ba5ccf
Clear008 <wissal-exo@hotmail.com>
2023-10-19 12:34:24 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,6 +8,6 @@ void print_beformain(void) __attribute__ ((constructor));
  */
 void print_beformain(void)
 {
-printf("You're beat! and yet, you must allow,\n
-I bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }
