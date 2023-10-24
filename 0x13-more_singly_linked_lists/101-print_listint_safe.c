14ecb9495423061aefccef9c73796b948e92d383
Clear008 <wissal-exo@hotmail.com>
2023-10-24 15:54:45 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -17,3 +17,19 @@ wcount++;
 }
 return (wcount);
 }
+/**
+ * c_node - function to creat new node
+ * @m: an integer
+ * Return: new node
+ */
+listint_t *c_node(int m)
+{
+listint_t *nw_node = malloc(sizeof(listint_t));
+if (nw_node == NULL)
+{
+exit(98);
+}
+nw_node->n = m;
+nw_node->next = NULL;
+return (nw_node);
+}
