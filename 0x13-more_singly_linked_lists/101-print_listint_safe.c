0ccbd5f2fe4758b0af0fa519d13a950245e8dcd7
Clear008 <wissal-exo@hotmail.com>
2023-10-24 16:43:19 -0700

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -7,29 +7,14 @@
 size_t print_listint_safe(const listint_t *head)
 {
 size_t  wcount = 0;
-
-while (head != NULL)
+const listint_t *t;
+t = *h;
+while (t != NULL)
 {
-printf("[%p] %d\n", (void *)head, head->n);
+printf("[%p] %d\n", (void *)t, t->n);
 
-head = head->next;
+t = t->next;
 wcount++;
 }
 return (wcount);
-}
-/**
- * c_node - function to creat new node
- * @m: an integer
- * Return: new node
- */
-listint_t *c_node(int m)
-{
-listint_t *nw_node = malloc(sizeof(listint_t));
-if (nw_node == NULL)
-{
-exit(98);
-}
-nw_node->n = m;
-nw_node->next = NULL;
-return (nw_node);
 }
