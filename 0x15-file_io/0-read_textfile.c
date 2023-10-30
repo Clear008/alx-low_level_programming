58b50f6a977ac46ab8c9cc5f9061bca390d184a5
Clear008 <wissal-exo@hotmail.com>
2023-10-30 10:44:01 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,7 +10,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 int filed, r;
 char *buff;
 if (filename == NULL)
-	return(0);
+	return (0);
 
 filed = open(filename, O_RDONLY);
 if (filed == -1)
