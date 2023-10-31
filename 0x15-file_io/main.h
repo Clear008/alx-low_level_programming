317c7eff33b9a0c574a1afd41f8d4307a4c78d70
Clear008 <wissal-exo@hotmail.com>
2023-10-31 07:37:49 -0700

    update main file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,10 +5,14 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-
+#include <elf.h>
 
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void print_ELF_Magic(const unsigned char e_ident[EI_NIDENT]);
+void print_ELF_Class(const unsigned char e_ident[EI_NIDENT]);
+void print_ELF_Data(const unsigned char e_ident[EI_NIDENT]);
+
 #endif
