b360c539ee29161a306c6c796e1628ef4fac611f
Clear008 <wissal-exo@hotmail.com>
2023-10-31 15:10:04 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -7,7 +7,7 @@
 * return: no return is a void func.
 */
 
-void print_ELF_Magic(const unsigned char e_ident[EI_NIDENT])
+void print_ELF_Magic(const unsigned char *e_ident)
 {
 int inx;
 printf("  Magic:   ");
@@ -18,33 +18,143 @@ printf("%02x ", e_ident[inx]);
 printf("\n");
 }
 /**
- * print_ELF_Class - print ELF class
- * @e_ident: ELF identifier
+ * print_ELF_Class - print ELF class.
+ * @e_ident: ELF identifier.
  */
-void print_ELF_Class(const unsigned char e_ident[EI_NIDENT])
+void print_ELF_Class(const unsigned char *e_ident)
 {
 printf("  Class:                             ");
 if (e_ident[EI_CLASS] == ELFCLASS32)
-{printf("ELF32\n");
-}
+printf("ELF32\n");
 else if (e_ident[EI_CLASS] == ELFCLASS64)
-{
 printf("ELF64\n");
-}
+else if (e_ident[EI_CLASS] == ELFCLASSNONE)
+printf("Invalid class\n");
 }
 /**
- * print_ELF_Data - print ELF data format
- * @e_ident: ELF identifier
+ * print_ELF_Data - print ELF data format.
+ * @e_ident: ELF identifier.
  */
-void print_ELF_Data(const unsigned char e_ident[EI_NIDENT])
+void print_ELF_Data(const unsigned char *e_ident)
 {
 printf("  Data:                              ");
 if (e_ident[EI_DATA] == ELFDATA2LSB)
 printf("2's complement, little endian\n");
 else if (e_ident[EI_DATA] == ELFDATA2MSB)
+{
 printf("2's complement, big endian\n");
-
 }
+else if (e_ident[EI_DATA] == ELFDATANONE)
+{
+printf("Invalid data encoding\n");
+}
+}
+/**
+* print_ELF_Version - print ELF version .
+* @e_ident: ELF identifier.
+*/
+void print_ELF_Version(unsigned char *e_ident)
+{
+	printf("  Version:                           %d",
+e_ident[EI_VERSION]);
+	if (e_ident[EI_VERSION] == EV_CURRENT)
+		printf(" (current)\n");
+	else
+		printf("\n");
+}
+/**
+ * print_ELF_OsAbi - print the OSABI.
+ * @e_ident: ELF identifier.
+ */
+void print_ELF_OsAbi(unsigned char *e_ident)
+{
+printf("  OS/ABI:                            ");
+if (e_ident[EI_OSABI] == ELFOSABI_NONE)
+printf("UNIX - System V\n");
+else if (e_ident[EI_OSABI] == ELFOSABI_HPUX)
+printf("UNIX - HP-UX\n");
+else if (e_ident[EI_OSABI] == ELFOSABI_NETBSD)
+printf("UNIX - NetBSD\n");
+else if (e_ident[EI_OSABI] == ELFOSABI_LINUX)
+printf("UNIX - Linux\n");
+else if (e_ident[EI_OSABI] == ELFOSABI_SOLARIS)
+printf("UNIX - Solaris\n");
+else if (e_ident[EI_OSABI] == ELFOSABI_AIX)
+printf("UNIX - AIX\n");
+else if (e_ident[EI_OSABI] == ELFOSABI_IRIX)
+printf("UNIX - IRIX\n");
+else if (e_ident[EI_OSABI] == ELFOSABI_FREEBSD)
+printf("UNIX - FreeBSD\n");
+else if (e_ident[EI_OSABI] == ELFOSABI_TRU64)
+printf("UNIX - TRU64\n");
+else if (e_ident[EI_OSABI] == ELFOSABI_ARM)
+printf("ARM\n");
+else if (e_ident[EI_OSABI] == ELFOSABI_STANDALONE)
+printf("Standalone App\n");
+else
+printf("<unknown: %x>\n", e_ident[EI_OSABI]);
+}
+/**
+ * print_ELF_Abiv - function that print the ABI version
+ * @e_ident: the ELF identifier.
+ */
+
+void print_ELF_Abiv(unsigned char *e_ident)
+{
+printf("  ABI Version:                       %d\n",
+e_ident[EI_ABIVERSION]);
+}
+
+/**
+* print_ELF_type - print the type
+* @e_ident: the ELF identifier.
+* @e_type: a data
+*/
+
+void print_ELF_type(unsigned int e_type, unsigned char *e_ident)
+{
+if (e_ident[EI_DATA] == ELFDATA2MSB)
+e_type >>= 8;
+
+printf("  Type:                              ");
+if (e_type == ET_NONE)
+printf("NONE (None)\n");
+else if (e_type == ET_REL)
+printf("REL (Relocatable file)\n");
+else if (e_type == ET_EXEC)
+printf("EXEC (Executable file)\n");
+else if (e_type == ET_DYN)
+printf("DYN (Shared object file)\n");
+else if (e_type == ET_CORE)
+printf("CORE (Core file)\n");
+else
+printf("<unknown: %x>\n", e_type);
+}
+
+/**
+* print_ELF_entry - ELF entry
+* @e_ident: the ELF identifier.
+* @e_entry: a data
+*/
+
+void print_ELF_entry(unsigned long int e_entry, unsigned char *e_ident)
+{
+printf("  Entry point address:               ");
+
+if (e_ident[EI_DATA] == ELFDATA2MSB)
+{
+e_entry = ((e_entry << 8) & 0xFF00FF00) |
+((e_entry >> 8) & 0xFF00FF);
+e_entry = (e_entry << 16) | (e_entry >> 16);
+}
+
+if (e_ident[EI_CLASS] == ELFCLASS32)
+printf("%#x\n", (unsigned int)e_entry);
+
+else
+printf("%#lx\n", e_entry);
+}
+
 
 /**
  *main - check the code
@@ -56,27 +166,33 @@ printf("2's complement, big endian\n");
 int main(int argc, char *argv[])
 {
 int fd, r;
-unsigned char elf_magic[EI_NIDENT];
+Elf64_Ehdr h;
+
 if (argc != 2)
 fprintf(stderr, "Usage: %s elf_filename\n", argv[0]), exit(98);
 fd = open(argv[1], O_RDONLY);
 if (fd == -1)
 fprintf(stderr, "Error: Can't open file %s\n", argv[1]), exit(98);
 
-r = read(fd, elf_magic, EI_NIDENT);
+r = read(fd, &h, sizeof(Elf64_Ehdr));
 if (r == -1)
 {
 	perror("Error: Read failed");
 close(fd);
 exit(98);
 }
-if (elf_magic[0] == 0x7F && elf_magic[1] == 'E' &&
-elf_magic[2] == 'L' && elf_magic[3] == 'F')
+if (h.e_ident[0] == 0x7F && h.e_ident[1] == 'E' &&
+h.e_ident[2] == 'L' && h.e_ident[3] == 'F')
 {
 printf("ELF Header:\n");
-print_ELF_Magic(elf_magic);
-print_ELF_Class(elf_magic);
-print_ELF_Data(elf_magic);
+print_ELF_Magic(h.e_ident);
+print_ELF_Class(h.e_ident);
+print_ELF_Data(h.e_ident);
+print_ELF_Version(h.e_ident);
+print_ELF_OsAbi(h.e_ident);
+print_ELF_Abiv(h.e_ident);
+print_ELF_type(h.e_type, h.e_ident);
+print_ELF_entry(h.e_entry, h.e_ident);
 }
 else
 {
@@ -89,3 +205,4 @@ close(fd);
 
 return (0);
 }
+
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,8 +11,13 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void print_ELF_Magic(const unsigned char e_ident[EI_NIDENT]);
-void print_ELF_Class(const unsigned char e_ident[EI_NIDENT]);
-void print_ELF_Data(const unsigned char e_ident[EI_NIDENT]);
+void print_ELF_Magic(const unsigned char *e_ident);
+void print_ELF_Class(const unsigned char *e_ident);
+void print_ELF_Data(const unsigned char *e_ident);
+void print_ELF_Version(unsigned char *e_ident);
+void print_ELF_OsAbi(unsigned char *e_ident);
+void print_ELF_Abiv(unsigned char *e_ident);
+void print_ELF_type(unsigned int e_type, unsigned char *e_ident);
+void print_ELF_entry(unsigned long int e_entry, unsigned char *e_ident);
 
 #endif
