a1d447430e9094b19cb89d17a02e494233750654
Clear008 <wissal-exo@hotmail.com>
2023-12-11 12:11:54 -0800

    static library

---
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_isdigit  - check if c is a lowercase
+ *@c :is a character to be checked
+ *Return: 1 if c is lowercase 0 otherwise
+*/
+
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+
+return (1);
+else
+return (0);
+}
