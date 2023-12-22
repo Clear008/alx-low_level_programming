4ba3436748a346d1974e77779e4f8f4584c69e34
Clear008 <wissal-exo@hotmail.com>
2023-12-22 09:24:48 -0800

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,19 +9,25 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
-	unsigned long int i;
+hash_table_t *hash_t = NULL;
+unsigned long int i;
 
-	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
-		return (NULL);
+hash_t = malloc(sizeof(hash_table_t));
+if (hash_t == NULL)
+return (NULL);
 
-	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
-		return (NULL);
-	for (i = 0; i < size; i++)
-		ht->array[i] = NULL;
+hash_t->array = malloc(sizeof(hash_node_t *) * size);
+if (hash_t->array == NULL)
+{
+free(hash_t);
+return (NULL);
+}
+for (i = 0; i < size; i++)
+{
+hash_t->array[i] = NULL;
+}
 
-	return (ht);
+hash_t->size = size;
+
+return (hash_t);
 }
