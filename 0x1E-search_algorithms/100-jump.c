31d6107833fb26535cacdceae9c85f1d8e4955cc
Clear008 <wissal-exo@hotmail.com>
2024-04-11 11:54:43 -0700

    Big O notation

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,34 @@
+#include "search_algos.h"
+/**
+ * jump_search - Function that searches for a value
+ * in an array using Jump search algorithm
+ * @array: Pointer to the first element of the array to search in
+ * @size: Number of elements in the array
+ * @value: Value to search for
+ * Return: Index of the first occurrence of value, or -1
+ */
+int jump_search(int *array, size_t size, int value)
+{
+size_t indx, jump, step;
+
+if (array == NULL || size == 0)
+return (-1);
+
+step = sqrt(size);
+for (indx = jump = 0; jump < size && array[jump] < value;)
+{
+printf("Value checked array[%ld] = [%d]\n", jump, array[jump]);
+indx = jump;
+jump += step;
+}
+
+printf("Value found between indexes [%ld] and [%ld]\n", indx, jump);
+
+jump = jump < size - 1 ? jump : size - 1;
+for (; indx < jump && array[i] < value; indx++)
+printf("Value checked array[%ld] = [%d]\n", indx, array[indx]);
+printf("Value checked array[%ld] = [%d]\n", indx, array[indx]);
+
+return (array[indx] == value ? (int)indx : -1);
+}
+
