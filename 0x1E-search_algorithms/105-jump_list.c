71856e3a6df01ae94abc8288962e422af9c576cb
Clear008 <wissal-exo@hotmail.com>
2024-04-11 20:07:06 -0700

    105-jump_list.c

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -2,7 +2,7 @@
 
 /**
  * jump_list - searches for a value in a singly linked list using
- *             the Jump search algorithm
+ * the Jump search algorithm
  * @list: pointer to the head of the linked list
  * @size: number of elements in the linked list
  * @value: value to search for
@@ -16,27 +16,29 @@ listint_t *prev;
 if (list == NULL || size == 0)
 return (NULL);
 c = (size_t)sqrt((double)size);
-while (list->next && list->n < value)
-{
+do {
 prev = list;
 n++;
-index = n * c;
+index = n *c;
 
 while (list->next && list->index < index)
 list = list->next;
 
-printf("Value checked at index [%lu] = [%d]\n", index, list->n);
-}
-printf("Value found between indexes ");
-printf("[%lu] and [%lu]\n", prev->index, list->index);
+if (list->next == NULL && index != list->index)
+index = list->index;
 
-while (prev && prev->index <= list->index)
+printf("Value checked at index [%d] = [%d]\n", (int)index, list->n);
+
+} while (index < size && list->next && list->n < value);
+printf("Value found between indexes ");
+printf("[%d] and [%d]\n", (int)prev->index, (int)list->index);
+for (; prev && prev->index <= list->index; prev = prev->next)
 {
-printf("Value checked at index [%lu] = [%d]\n", prev->index, prev->n);
+printf("Value checked at index [%d] = [%d]\n", (int)prev->index, prev->n);
 if (prev->n == value)
 return (prev);
-prev = prev->next;
 }
+
 return (NULL);
 }
 
