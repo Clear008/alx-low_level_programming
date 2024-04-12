5d1cc55882779b4e45902f9064b05e16dc9123ce
Clear008 <wissal-exo@hotmail.com>
2024-04-11 19:57:34 -0700

    105-jump_list.c

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,44 +1,45 @@
 #include "search_algos.h"
-/**
- * linear_skip - searches for a value in an array of
- * integers using the linear search algorithm
- * @list: a pointer to the first element of the array to search in
- * @value: value to search for
- * Return: index of the number
 
+/**
+ * linear_skip - searches for a value in a skip list
+ *
+ * @list: input list
+ * @value: value to search in
+ * Return: index of the number
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-skiplist_t *pr;
-if (list == NULL)
-return (NULL);
-pr = list;
+	skiplist_t *go;
 
-do {
-list = pr;
-pr = go->express;
-printf("Value checked at index ");
-printf("[%d] = [%d]\n", (int)go->index, go->n);
-} while (go->express && pr->n < value);
+	if (list == NULL)
+		return (NULL);
 
-if (go->express == NULL)
-{
-list = pr;
-while (go->next)
-pr = go->next;
+	go = list;
+
+	do {
+		list = go;
+		go = go->express;
+		printf("Value checked at index ");
+		printf("[%d] = [%d]\n", (int)go->index, go->n);
+	} while (go->express && go->n < value);
+
+	if (go->express == NULL)
+	{
+		list = go;
+		while (go->next)
+			go = go->next;
+	}
+
+	printf("Value found between indexes ");
+	printf("[%d] and [%d]\n", (int)list->index, (int)go->index);
+
+	while (list != go->next)
+	{
+		printf("Value checked at index [%d] = [%d]\n", (int)list->index, list->n);
+		if (list->n == value)
+			return (list);
+		list = list->next;
+	}
+
+	return (NULL);
 }
-
-printf("Value found between indexes ");
-printf("[%d] and [%d]\n", (int)list->index, (int)go->index);
-
-while (list != go->next)
-{
-printf("Value checked at index [%d] = [%d]\n", (int)list->index, list->n);
-if (list->n == value)
-return (list);
-list = list->next;
-}
-
-return (NULL);
-}
-
